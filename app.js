e610be00608070b14688d84195137f9816af5261
Am <arphasirisubb@gmail.com>
2024-01-16 13:24:30 +0700

    feat: creating APIs on Express with MongoDB

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
 import express from "express";
+import questionRouter from "./app/questions.js";
+
+import { client } from "./utils/db.js";
 
 async function init() {
   const app = express();
   const port = 4000;
 
+  try {
+    await client.connect();
+  } catch {
+    console.log("Cannot connect database");
+  }
+
   app.use(express.json());
-  app.use(express.urlencoded({ extended: false }));
+  app.use(express.urlencoded({ extended: true }));
+
+  app.use("/questions", questionRouter);
 
   app.get("/", (req, res) => {
     return res.json("Hello Skill Checkpoint #2");
